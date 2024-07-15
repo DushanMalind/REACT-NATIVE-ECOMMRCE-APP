2f81cf9a84732067ab1004de8ae2bbf899647dec
DushanMalind <dushanmalinda010@gmail.com>
2024-07-15 13:05:05 +0530

    bug fix done all run

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Dushan</Text>
+   /* <View style={styles.container}>*/
+      <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-[40px] text-green-400">Welcome Dushan</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
