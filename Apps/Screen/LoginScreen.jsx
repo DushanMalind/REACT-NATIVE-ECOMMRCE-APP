1bdbe97ea1b5632fa9c19a3740cd01e7469eb73b
DushanMalind <dushanmalinda010@gmail.com>
2024-07-16 12:01:56 +0530

    LoginScreen home market

--- Apps/Screen/LoginScreen.jsx
+++ Apps/Screen/LoginScreen.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Image, Text, View} from 'react-native';
 
 export default function LoginScreen() {
     return (
         <View>
-            <Text>LoginScreen</Text>
+            <Image source={require('../../assets/images/login.jpg')}
+             className="w-full h-[400px] object-cover"/>
+            <View className="p-8">
+                <Text className="text-[30px] font-bold">Home Market</Text>
+                <Text className="text-[18px] text-slate-600 mt-6">You Can Sell And By product.Make Real Money</Text>
+                <View className="p-4 bg-amber-400 rounded-full mt-20">
+                    <Text className="text-black text-center text-[18px]">Get Started</Text>
+                    {/*<Button title={"hr"}></Button>*/}
+                </View>
+            </View>
         </View>
     );
 }
