b57be0705bb016eacad0e46b3237ebc206935de6
DushanMalind <dushanmalinda010@gmail.com>
2024-07-16 16:44:25 +0530

    useWarmUpBrowser and warmUpBrowser Create

--- App.js
+++ App.js
@@ -2,15 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
 import { Text, View } from 'react-native';
 import LoginScreen from "./Apps/Screen/LoginScreen";
+import {ClerkProvider, SignedIn, SignedOut} from "@clerk/clerk-expo";
 
 export default function App() {
   return (
    /* <View style={styles.container items-center justify-center }>*/
-      <View className="flex-1 bg-white">
-      {/*<Text className="text-[40px] text-green-400">Welcome Dushan</Text>*/}
-      <StatusBar style="auto" />
-          <LoginScreen/>
-    </View>
+      <ClerkProvider publishableKey='pk_test_dG91Y2hlZC1yYXQtOTUuY2xlcmsuYWNjb3VudHMuZGV2JA'>
+          <View className="flex-1 bg-white">
+              {/*<Text className="text-[40px] text-green-400">Welcome Dushan</Text>*/}
+              <StatusBar style="auto" />
+
+              <SignedIn>
+                  <Text>Hello</Text>
+              </SignedIn>
+              <SignedOut>
+                  <LoginScreen/>
+              </SignedOut>
+          </View>
+      </ClerkProvider>
   );
 }
 
--- Apps/Screen/LoginScreen.jsx
+++ Apps/Screen/LoginScreen.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import {Button, Image, Text, TouchableOpacity, View} from 'react-native';
+import * as WebBrowser from "expo-web-browser";
+import {useOAuth} from "@clerk/clerk-expo";
+import {useWarmUpBrowser} from "../../hooks/warmUpBrowser";
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+    useWarmUpBrowser();
+
+    const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+    const onPress = React.useCallback(async () => {
+        try {
+            const { createdSessionId, signIn, signUp, setActive } =
+                await startOAuthFlow();
+
+            if (createdSessionId) {
+                 setActive({session: createdSessionId});
+            } else {
+                // Use signIn or signUp for next steps such as MFA
+            }
+        } catch (err) {
+            console.error("OAuth error", err);
+        }
+    }, []);
+
     return (
         <View>
             <Image source={require('../../assets/images/login.jpg')}
@@ -9,7 +32,7 @@ export default function LoginScreen() {
             <View className="p-8 bg-white mt-[-20px] rounded-t-3xl shadow-md">
                 <Text className="text-[30px] font-bold">Home Market</Text>
                 <Text className="text-[18px] text-slate-600 mt-6">You Can Sell And By product.Make Real Money</Text>
-                <TouchableOpacity onPress={()=>console.log("SignIn")} className="p-4 bg-amber-400 rounded-full mt-20">
+                <TouchableOpacity onPress={onPress} className="p-4 bg-amber-400 rounded-full mt-20">
                     <Text className="text-black text-center text-[18px]">Get Started</Text>
                     {/*<Button title={"hr"}></Button>*/}
                 </TouchableOpacity>
