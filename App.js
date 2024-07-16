b6ee8095b0a2f784617dfeacb7fcd462757b05ea
DushanMalind <dushanmalinda010@gmail.com>
2024-07-16 10:48:35 +0530

    LoginScreen create

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
 import { Text, View } from 'react-native';
+import LoginScreen from "./Apps/Screen/LoginScreen";
 
 export default function App() {
   return (
-   /* <View style={styles.container}>*/
-      <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-[40px] text-green-400">Welcome Dushan</Text>
+   /* <View style={styles.container items-center justify-center }>*/
+      <View className="flex-1 bg-white">
+      {/*<Text className="text-[40px] text-green-400">Welcome Dushan</Text>*/}
       <StatusBar style="auto" />
+          <LoginScreen/>
     </View>
   );
 }
