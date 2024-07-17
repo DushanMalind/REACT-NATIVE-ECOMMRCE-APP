0117ff3ce1f40a46f9f9b59bd48af12517754363
DushanMalind <dushanmalinda010@gmail.com>
2024-07-17 16:35:05 +0530

    TabNavigation Create Done

---
+++ Apps/Navigations/TabNavigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function TabNavigation() {
+    return (
+        <View>
+            <Text>TabNavigation</Text>
+        </View>
+    );
+}
