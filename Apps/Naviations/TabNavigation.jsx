a1498d2ea8634d6ef21fc310b82dbd445ad4b7e6
DushanMalind <dushanmalinda010@gmail.com>
2024-07-17 21:13:52 +0530

    screenOptions Done

--- Apps/Naviations/TabNavigation.jsx
+++ Apps/Naviations/TabNavigation.jsx
@@ -11,7 +11,7 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigation(){
     return (
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{headerShown:false}}>
             <Tab.Screen name="home" component={HomeScreen}/>
             <Tab.Screen name="explore" component={ExploreScreen}/>
             <Tab.Screen name="addPost" component={AddPostScreen}/>
