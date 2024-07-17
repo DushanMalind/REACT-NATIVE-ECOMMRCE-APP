9067f851a739352e2decace8db0acb10c52d80f8
DushanMalind <dushanmalinda010@gmail.com>
2024-07-17 16:31:18 +0530

    AddPostScreen Create

---
+++ Apps/Screen/AddPostScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function AddPostScreen() {
+    return (
+        <View>
+            <Text>AddPostScreen()</Text>
+        </View>
+    );
+}
