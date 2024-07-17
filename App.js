a3f165f1d51d9880e0d39fec2577e5799d74cc81
DushanMalind <dushanmalinda010@gmail.com>
2024-07-17 19:28:47 +0530

    LoginScreen Update

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from "./Apps/Screens/LoginScreen";
 
 export default function App() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-[40px] text-green-400">Open up App.js to start working on your app!</Text>
+      <View className="flex-1  bg-white">
       <StatusBar style="auto" />
+          <LoginScreen/>
     </View>
   );
 }
 
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
+
--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 
 export default function LoginScreen() {
     return (
         <View>
-            <Text>LoginScreen</Text>
+            <Image source={require('../../assets/images/login.jpg')}
+            className="w-full h-[400px] object-cover"
+            />
+            <View className="p-8">
+                <Text className="text-[30px] font-bold">Home Market</Text>
+                <Text className="text-[18px] text-gray-500 mt-6">You can
+                Sell Items, Buy Items, Make a Wishlist and many more.
+                </Text>
+                <TouchableOpacity className="p-4 bg-amber-400 rounded-full mt-20">
+                    <Text className="text-center text-[18px]">Get Stared</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
