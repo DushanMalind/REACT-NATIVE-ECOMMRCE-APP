8a322fdded6144c4245ea68587407566c9c7a833
DushanMalind <dushanmalinda010@gmail.com>
2024-07-17 19:15:54 +0530

    full version update

--- App.js
+++ App.js
@@ -1,37 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-import { Text, View } from 'react-native';
-import LoginScreen from "./Apps/Screen/LoginScreen";
-import {ClerkProvider, SignedIn, SignedOut} from "@clerk/clerk-expo";
-import {NavigationContainer} from "@react-navigation/native";
-import TabNavigation from "./Apps/Navigations/TabNavigation";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-   /* <View style={styles.container items-center justify-center }>*/
-      <ClerkProvider publishableKey='pk_test_dG91Y2hlZC1yYXQtOTUuY2xlcmsuYWNjb3VudHMuZGV2JA'>
-          <View className="flex-1 bg-white">
-              {/*<Text className="text-[40px] text-green-400">Welcome Dushan</Text>*/}
-              <StatusBar style="auto" />
-
-              <SignedIn>
-                  <NavigationContainer>
-                      <TabNavigation/>
-                  </NavigationContainer>
-              </SignedIn>
-              <SignedOut>
-                  <LoginScreen/>
-              </SignedOut>
-          </View>
-      </ClerkProvider>
+      <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-[40px] text-green-400">Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-/*const styles = StyleSheet.create({
+/*
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});*/
+});
+*/
