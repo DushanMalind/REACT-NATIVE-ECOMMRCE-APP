e56056dc8712c192bd5c20c0f90b79afcb38b0df
DushanMalind <dushanmalinda010@gmail.com>
2024-07-17 16:40:07 +0530

    TabNavigation route part create

--- Apps/Navigations/TabNavigation.jsx
+++ Apps/Navigations/TabNavigation.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "../Screen/HomeScreen";
+import ExploreScreen from "../Screen/ExploreScreen";
+import AddPostScreen from "../Screen/AddPostScreen";
+import ProfileScreen from "../Screen/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
     return (
-        <View>
-            <Text>TabNavigation</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="home" component={HomeScreen}></Tab.Screen>
+            <Tab.Screen name="explore" component={ExploreScreen}></Tab.Screen>
+            <Tab.Screen name="addpost" component={AddPostScreen}></Tab.Screen>
+            <Tab.Screen name="profile" component={ProfileScreen}></Tab.Screen>
+        </Tab.Navigator>
     );
 }
