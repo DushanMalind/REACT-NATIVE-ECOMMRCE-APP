62f5d5f74c5a81f3d5c8f3da2baa6d21dd5dd21e
DushanMalind <dushanmalinda010@gmail.com>
2024-07-17 20:13:01 +0530

    login success

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import LoginScreen from "./Apps/Screens/LoginScreen";
+import {ClerkProvider, SignedIn, SignedOut} from "@clerk/clerk-expo";
 
 export default function App() {
   return (
+      <ClerkProvider publishableKey='pk_test_ZHluYW1pYy1nYW5uZXQtMjIuY2xlcmsuYWNjb3VudHMuZGV2JA'>
       <View className="flex-1  bg-white">
       <StatusBar style="auto" />
-          <LoginScreen/>
-    </View>
+          <SignedIn>
+              <Text>Login</Text>
+          </SignedIn>
+          <SignedOut>
+              <LoginScreen />
+          </SignedOut>
+
+      </View>
+      </ClerkProvider>
   );
 }
 
--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import {Image, Text, TouchableOpacity, View} from 'react-native';
+import * as WebBrowser from "expo-web-browser";
+import {useWarmUpBrowser} from "../../hooks/warmUpBrowser";
+import {useOAuth} from "@clerk/clerk-expo";
+
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+    useWarmUpBrowser();
+
+    const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+    const onPress = React.useCallback(async () => {
+        try {
+            const { createdSessionId, signIn, signUp, setActive } =
+                await startOAuthFlow();
+
+            if (createdSessionId) {
+                setActive({session: createdSessionId});
+            } else {
+                // Use signIn or signUp for next steps such as MFA
+            }
+        } catch (err) {
+            console.error("OAuth error", err);
+        }
+    }, []);
+
+
     return (
         <View>
             <Image source={require('../../assets/images/login.jpg')}
             className="w-full h-[400px] object-cover"
             />
-            <View className="p-8">
+            <View className="p-8 bg-amber-100  mt-[-20px] rounded-t-3xl shadow-md">
                 <Text className="text-[30px] font-bold">Home Market</Text>
                 <Text className="text-[18px] text-gray-500 mt-6">You can
                 Sell Items, Buy Items, Make a Wishlist and many more.
                 </Text>
-                <TouchableOpacity className="p-4 bg-amber-400 rounded-full mt-20">
+                <TouchableOpacity onPress={onPress} className="p-4 bg-amber-400 rounded-full mt-20">
                     <Text className="text-center text-[18px]">Get Stared</Text>
                 </TouchableOpacity>
             </View>
