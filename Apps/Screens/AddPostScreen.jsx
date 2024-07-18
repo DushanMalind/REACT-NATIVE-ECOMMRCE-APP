6c6e3d489d968f95e14578a435604687f931426f
DushanMalind <dushanmalinda010@gmail.com>
2024-07-18 16:59:41 +0530

    getCategoryList logout

--- Apps/Screens/AddPostScreen.jsx
+++ Apps/Screens/AddPostScreen.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 
+import { getFirestore,getDocs,collection } from "firebase/firestore";
+import {app} from "../../firebaseConfig";
+
+
+
 export default function AddPostScreen() {
+
+    const db = getFirestore(app);
+
+    useEffect(()=>{
+        getCategoryList();
+    },[]);
+
+    const getCategoryList= async () => {
+        const querySnapshot = await getDocs(collection(db,'Category'));
+
+        querySnapshot.forEach((doc)=>{
+            // console.log(doc.id,doc.data());
+            console.log("Docs:",doc.data());
+        })
+    }
+
     return (
         <View>
             <Text>AddPostScreen</Text>
