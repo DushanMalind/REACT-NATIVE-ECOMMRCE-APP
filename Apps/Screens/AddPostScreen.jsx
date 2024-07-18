ecbbf6f5f79daa0e257e81c84e5f334a2aca2cea
DushanMalind <dushanmalinda010@gmail.com>
2024-07-18 17:36:12 +0530

    initialValues set

--- Apps/Screens/AddPostScreen.jsx
+++ Apps/Screens/AddPostScreen.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
 
 import { getFirestore,getDocs,collection } from "firebase/firestore";
 import {app} from "../../firebaseConfig";
+import {Formik} from "formik";
 
 
 
@@ -26,8 +27,39 @@ export default function AddPostScreen() {
     }
 
     return (
-        <View>
-            <Text>AddPostScreen</Text>
+        <View className="p-10">
+            <Formik
+                initialValues={{title:'',desc:'',categort:'',address:'',price:'',image:''}}
+                onSubmit={values => console.log(values)}
+            >
+                {({handleChange,handleBlur,handleSubmit,values})=>(
+                   <View>
+                       <TextInput
+                           style={styles.input}
+                           placeholder="Title"
+                           value={values?.title}
+                           onChangeText={handleChange('title')}
+                       />
+
+                       <Button onPress={handleSubmit}
+                               className="mt-7"
+                               title="submit"/>
+
+                   </View>
+                )}
+            </Formik>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input:{
+        borderWidth:1,
+        padding:10,
+        fontSize:17,
+        borderRadius:10,
+        paddingHorizontal:17,
+
+
+    }
+});
