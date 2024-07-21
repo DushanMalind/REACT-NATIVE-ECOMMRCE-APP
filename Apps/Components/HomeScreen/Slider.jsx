d47029f1d6e01f20852eba7ca0369a1151c948f6
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 15:43:43 +0530

    slider Done

--- Apps/Components/HomeScreen/Slider.jsx
+++ Apps/Components/HomeScreen/Slider.jsx
@@ -1,15 +1,29 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, Image, Text, View} from 'react-native';
 
 
 export default function Slider({sliderList}) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(prevIndex => (prevIndex + 1) % sliderList.length);
+        }, 3000); // Change slide every 3 seconds
+
+        return () => clearInterval(interval);
+    }, [sliderList.length]);
+
     return (
-        <View>
+        <View className="mt-5">
             <FlatList
                 data={sliderList}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
                 renderItem={({item,index}) => (
                     <View>
-                        <Text>{index}</Text>
+                        <Image source={{uri:item?.image}}
+                               className="w-[330px] h-[200px] object-contain mr-3 rounded-lg"
+                        />
                     </View>
                 )}
             />
