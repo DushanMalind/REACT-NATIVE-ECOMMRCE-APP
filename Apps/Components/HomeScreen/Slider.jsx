f0e6623f0b3117d2504ae854f33f522319ca6e42
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 16:03:45 +0530

    slider Update Done and Fix

--- Apps/Components/HomeScreen/Slider.jsx
+++ Apps/Components/HomeScreen/Slider.jsx
@@ -1,17 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {FlatList, Image, Text, View} from 'react-native';
 
 
 export default function Slider({sliderList}) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const flatListRef = useRef();
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex(prevIndex => (prevIndex + 1) % sliderList.length);
-        }, 3000); // Change slide every 3 seconds
+        if (sliderList.length > 0) {
+            const interval = setInterval(() => {
+                setCurrentIndex(prevIndex => (prevIndex + 1) % sliderList.length);
+            }, 3000); // Change slide every 3 seconds
 
-        return () => clearInterval(interval);
-    }, [sliderList.length]);
+            return () => clearInterval(interval); // Clean up the interval on component unmount
+        }
+    }, [sliderList]);
+
+    useEffect(() => {
+        if (flatListRef.current && sliderList.length > 0) {
+            flatListRef.current.scrollToIndex({ animated: true, index: currentIndex });
+        }
+    }, [currentIndex, sliderList]);
+
+    if (sliderList.length === 0) {
+        return <Text className="text-[16px] text-center">No items to display</Text>;
+    }
 
     return (
         /*<View className="mt-5">
@@ -31,10 +44,11 @@ export default function Slider({sliderList}) {
         </View>*/
         <View className="mt-5">
             <FlatList
+                ref={flatListRef}
                 data={sliderList}
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
-                renderItem={({item, index}) => (
+                renderItem={({item}) => (
                     <View>
                         <Image source={{uri: item?.image}}
                                className="w-[330px] h-[200px] object-contain mr-3 rounded-lg"
@@ -42,13 +56,7 @@ export default function Slider({sliderList}) {
                     </View>
                 )}
                 keyExtractor={(item, index) => index.toString()}
-                extraData={currentIndex}
                 onScrollToIndexFailed={() => {}}
-                ref={(ref) => {
-                    if (ref) {
-                        ref.scrollToIndex({ animated: true, index: currentIndex });
-                    }
-                }}
             />
         </View>
     );
