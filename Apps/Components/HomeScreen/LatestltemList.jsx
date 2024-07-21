23705ade31817e30b62f93abf56d072ad31c0b62
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 14:13:57 +0530

    LatestltemList create

---
+++ Apps/Components/HomeScreen/LatestltemList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function LatestltemList() {
+    return (
+        <View>
+            <Text>LatestltemList()</Text>
+        </View>
+    );
+}
