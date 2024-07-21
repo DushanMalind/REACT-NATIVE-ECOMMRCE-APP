dc27135ee84ac42ad34167bed6f02bc6c4b33823
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 17:07:08 +0530

    TouchableOpacity set item

--- Apps/Components/HomeScreen/Categories.jsx
+++ Apps/Components/HomeScreen/Categories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList, Image, Text, View} from 'react-native';
+import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
 
 export default function Categories({categoryList}) {
     return (
@@ -9,12 +9,12 @@ export default function Categories({categoryList}) {
                 data={categoryList}
                 numColumns={4}
                 renderItem={({item, index}) => (
-                    <View className="flex-1 items-center justify-center p-2 border-[1px] border-amber-300 m-1 h-[80px] rounded-lg">
+                    <TouchableOpacity className="flex-1 items-center justify-center p-2 border-[1px] border-amber-300 m-1 h-[80px] rounded-lg">
                         <Image source={{uri: item.icon}}
                                className="w-[40px] h-[40px]"
                         />
                         <Text className="text-[12px] mt-1">{item.name}</Text>
-                    </View>
+                    </TouchableOpacity>
                 )}
             />
         </View>
