651da3e6ab8863767a6701a51de6ceff66bda798
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 18:52:35 +0530

    HomeScreeStackNav create

---
+++ Apps/Naviations/HomeScreeStackNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function HomeScreeStackNav() {
+    return (
+        <View>
+            <Text>HomeScreeStackNav</Text>
+        </View>
+    );
+}
