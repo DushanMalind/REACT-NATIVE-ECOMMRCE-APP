18bef17eebb25c7bb27b573091609102f8636b33
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 15:13:51 +0530

    getSliders and List log

--- Apps/Components/HomeScreen/Slider.jsx
+++ Apps/Components/HomeScreen/Slider.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import {app} from "../../../firebaseConfig";
-import {getFirestore} from "firebase/firestore";
+import {collection, getDocs, getFirestore} from "firebase/firestore";
 
 export default function Slider() {
     const db = getFirestore(app);
-    const getSliders=()=>{
+    const [sliderList, setSliderList] = useState([]);
 
+    useEffect(()=>{
+        getSliders();
+    },[]);
+
+    /*
+    * Get all sliders from firebase
+    * */
+
+    const getSliders=async ()=>{
+        const querySnapshot = await getDocs(collection(db, "Sliders"));
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
     }
 
     return (
