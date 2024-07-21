0bdbc588d5d3ee58d67adbd6773f66d0ca4c8ee2
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 18:47:45 +0530

    PostItem list item

--- Apps/Components/HomeScreen/PostItem.jsx
+++ Apps/Components/HomeScreen/PostItem.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 
-export default function PostItem() {
+export default function PostItem({item}) {
     return (
-        <View>
-            <Text>PostItem</Text>
-        </View>
+        <TouchableOpacity className="flex-1 m-2 p-2 rounded-lg border-[1px] border-slate-200">
+            <Image source={{uri:item.image}}
+                   className="w-full h-[140px] rounded-lg"
+            />
+            <View>
+                <Text className="text-[15px] font-bold mt-2">{item.title}</Text>
+                {/*<Text className="text-[12px] text-gray-500">{item.desc}</Text>*/}
+                <Text className="text-[18px] font-bold bg-yellow">$ {item.price}</Text>
+                <Text className="text-[10px] mt-1 text-amber-400 text-center bg-amber-100 p-[2px] rounded-full px-1 w-[70px]">{item.category}</Text>
+            </View>
+        </TouchableOpacity>
     );
 }
