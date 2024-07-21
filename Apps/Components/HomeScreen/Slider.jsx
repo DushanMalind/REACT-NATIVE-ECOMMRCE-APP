60f46d3b66af64152d16470c8f1ba8ecdecc31f4
DushanMalind <dushanmalinda010@gmail.com>
2024-07-21 15:21:11 +0530

    getSliders Home set

--- Apps/Components/HomeScreen/Slider.jsx
+++ Apps/Components/HomeScreen/Slider.jsx
@@ -1,27 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
-import {app} from "../../../firebaseConfig";
-import {collection, getDocs, getFirestore} from "firebase/firestore";
+
 
 export default function Slider() {
-    const db = getFirestore(app);
-    const [sliderList, setSliderList] = useState([]);
 
-    useEffect(()=>{
-        getSliders();
-    },[]);
-
-    /*
-    * Get all sliders from firebase
-    * */
-
-    const getSliders=async ()=>{
-        const querySnapshot = await getDocs(collection(db, "Sliders"));
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    }
 
     return (
         <View>
--- Apps/Screens/HomeScreen.jsx
+++ Apps/Screens/HomeScreen.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import Header from "../Components/HomeScreen/Header";
 import Slider from "../Components/HomeScreen/Slider";
+import {collection, getDocs, getFirestore} from "firebase/firestore";
+import {app} from "../../firebaseConfig";
+
 
 export default function HomeScreen(){
+    const db = getFirestore(app);
+    const [sliderList, setSliderList] = useState([]);
+
+    useEffect(()=>{
+        getSliders();
+    },[]);
+
+    /*
+    * Get all sliders from firebase
+    * */
+
+    const getSliders=async ()=>{
+        const querySnapshot = await getDocs(collection(db, "Sliders"));
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            /*console.log(doc.id, " => ", doc.data());*/
+            setSliderList(sliderList=>[...sliderList, doc.data()]);
+        });
+    }
     return (
         <View className="py-10 px-6 bg-white flex-1">
             <Header/>
