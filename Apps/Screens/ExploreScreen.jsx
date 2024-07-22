42a593323827d476feb805a3c261436ec308360b
DushanMalind <dushanmalinda010@gmail.com>
2024-07-22 15:37:01 +0530

    Explore getAllProducts

--- Apps/Screens/ExploreScreen.jsx
+++ Apps/Screens/ExploreScreen.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import {collection, getDocs, getFirestore, orderBy, query} from "firebase/firestore";
+import {app} from "../../firebaseConfig";
 
 export default function ExploreScreen() {
+    const db = getFirestore(app);
+
+    useEffect(()=>{
+        getAllProducts();
+    },[]);
+
+    const getAllProducts=async ()=>{
+        const q = query(collection(db, 'UserPost'),orderBy('createdAt','desc'));
+        const snapshot =await getDocs(q);
+        snapshot.forEach((doc)=>{
+            console.log(doc.data());
+        });
+    }
+
+
+
     return (
-        <View>
-            <Text>ExploreScreen</Text>
+        <View className="p-5 py-8">
+            <Text className="text-[24px] font-bold">Explore Other</Text>
         </View>
     );
 }
