fdb1fd568acf925c11824cb5c09f5646f1aeaac1
DushanMalind <dushanmalinda010@gmail.com>
2024-07-22 12:10:07 +0530

    Stack Navigator

--- Apps/Naviations/HomeScreeStackNav.jsx
+++ Apps/Naviations/HomeScreeStackNav.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import HomeScreen from "../Screens/HomeScreen";
+
+const Stack = createStackNavigator();
 
 export default function HomeScreeStackNav() {
     return (
-        <View>
-            <Text>HomeScreeStackNav</Text>
-        </View>
+        <Stack.Navigator>
+            <Stack.Screen name='home' component={HomeScreen}/>
+        </Stack.Navigator>
     );
 }
