9b17e7df6d833de759bb8083c33ba65ded049393
DushanMalind <dushanmalinda010@gmail.com>
2024-07-22 15:20:52 +0530

    itemlist loading

--- Apps/Screens/ItemList.jsx
+++ Apps/Screens/ItemList.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 import {useRoute} from "@react-navigation/native";
 import {collection, getDocs, getFirestore, query, where} from "firebase/firestore";
 import {app} from "../../firebaseConfig";
@@ -9,6 +9,7 @@ export default function ItemList() {
     const {params}=useRoute();
     const db = getFirestore(app);
     const [itemList, setItemList] = useState([]);
+    const [loading,setLoading]=useState(false);
 
     useEffect(()=>{
         /*console.log("Category:",params.category);*/
@@ -17,17 +18,22 @@ export default function ItemList() {
 
     const getItemListByCategory=async ()=>{
         setItemList([]);
+        setLoading(true);
         const q=query(collection(db,'UserPost'),where('category','==',params.category));
         const snapshot = await getDocs(q);
         snapshot.forEach((doc)=>{
             console.log(doc.data());
             setItemList(itemList=>[...itemList,doc.data()]);
+            setLoading(false);
         });
     }
 
     return (
         <View className="p-2">
-            {itemList?.length>0?
+            {loading?
+                <ActivityIndicator size='large' color='#FFCA4B'/>
+                :
+            itemList?.length>0?
             <LatestltemList latestItemList={itemList}
                             heading={'Best Post'}
             />
