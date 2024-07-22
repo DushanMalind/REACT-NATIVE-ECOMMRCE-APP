e45f9c2ab87b23ce5ecb49687e53fd740533845b
DushanMalind <dushanmalinda010@gmail.com>
2024-07-22 16:47:22 +0530

    ProfileScreen FlatList set

--- Apps/Screens/ProfileScreen.jsx
+++ Apps/Screens/ProfileScreen.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
 import {useUser} from "@clerk/clerk-expo";
-
+import img from "../../assets/images/post.png";
+import explore from "../../assets/images/explore.png";
+import details from "../../assets/images/details.png";
+import logout from "../../assets/images/logout.png";
 export default function ProfileScreen() {
 
     const {user}=useUser();
+
+    const menuList=[
+        {
+            id:1,
+            name:'My Post',
+            icon:img
+        },
+        {
+            id:2,
+            name:'Explore',
+            icon:explore
+        },
+        {
+            id:3,
+            name:'Me Details',
+            icon:details
+        },
+        {
+            id:4,
+            name:'LogOut',
+            icon:logout
+        }
+    ]
 
     return (
         <View className="p-5 py-8">
@@ -15,6 +41,22 @@ export default function ProfileScreen() {
             <Text className="text-[24px] font-bold mt-3">{user?.fullName}</Text>
             <Text className="text-[18px] text-gray-500">{user?.primaryEmailAddress?.emailAddress}</Text>
             </View>
+
+            <FlatList
+                data={menuList}
+                numColumns={3}
+                style={{marginTop:20}}
+                renderItem={({item, index}) => (
+                     <TouchableOpacity className="flex-1 p-5 border-[1px] items-center m-4 rounded-lg border-amber-300
+                     bg-amber-200 mx-4 mt-4">
+                         {item.icon&& <Image source={item?.icon}
+                         className="w-[50px] h-[50px]"
+                         />}
+                         <Text>{item.name}</Text>
+                     </TouchableOpacity>
+                )}
+            />
+
         </View>
     );
 }
