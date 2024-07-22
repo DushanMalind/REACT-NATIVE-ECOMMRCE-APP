485aa985210f72dcfb87f105c4ef51a9b3c24159
DushanMalind <dushanmalinda010@gmail.com>
2024-07-22 15:43:50 +0530

    Explore LatestltemList set

--- Apps/Screens/ExploreScreen.jsx
+++ Apps/Screens/ExploreScreen.jsx
@@ -1,28 +1,33 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ScrollView, Text, View} from 'react-native';
 import {collection, getDocs, getFirestore, orderBy, query} from "firebase/firestore";
 import {app} from "../../firebaseConfig";
+import LatestltemList from "../Components/HomeScreen/LatestltemList";
 
 export default function ExploreScreen() {
     const db = getFirestore(app);
+    const [productList,setProductList]=useState([]);
 
     useEffect(()=>{
         getAllProducts();
     },[]);
 
     const getAllProducts=async ()=>{
+        setProductList([]);
         const q = query(collection(db, 'UserPost'),orderBy('createdAt','desc'));
         const snapshot =await getDocs(q);
         snapshot.forEach((doc)=>{
-            console.log(doc.data());
+            /*console.log(doc.data());*/
+            setProductList(productList=>[...productList,doc.data()]);
         });
     }
 
 
 
     return (
-        <View className="p-5 py-8">
+        <ScrollView className="p-5 py-8">
             <Text className="text-[24px] font-bold">Explore Other</Text>
-        </View>
+            <LatestltemList latestItemList={productList}/>
+        </ScrollView>
     );
 }
