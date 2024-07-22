7d446e3fcfaa0866d53b6a43926b349170ee4066
DushanMalind <dushanmalinda010@gmail.com>
2024-07-22 16:17:05 +0530

    ProfileScreen user Set

--- Apps/Screens/ProfileScreen.jsx
+++ Apps/Screens/ProfileScreen.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import {useUser} from "@clerk/clerk-expo";
 
 export default function ProfileScreen() {
+
+    const {user}=useUser();
+
     return (
-        <View>
-            <Text>ProfileScreen</Text>
+        <View className="p-5 py-8">
+            <View className="items-center mt-5">
+            <Image source={{uri:user?.imageUrl}}
+                     className="w-[100px] h-[100px] rounded-full"
+            />
+            <Text className="text-[24px] font-bold mt-3">{user?.fullName}</Text>
+            <Text className="text-[18px] text-gray-500">{user?.primaryEmailAddress?.emailAddress}</Text>
+            </View>
         </View>
     );
 }
